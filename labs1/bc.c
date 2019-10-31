9412851a112bb74bb398f8b2212822c1d270d9e3
NIKE-ADIDASA <a117163086@126.com>
2019-10-31 07:33:30 -0700

    added a bc.c lile

--- labs1/bc.c
+++ labs1/bc.c
@@ -7,20 +7,17 @@ int main(void){
     int count = 0;
     printf ("\n");
     scanf ("%d",&a);
-    printf("");
     while (a){
         c[b] = a%2;
         b++;
         a/=2;
     }
     for (int j = b-1;j>=0;j--){
-        printf("%d",c[j]);
         if(c[j]==1){
             count++;
         }
     }
-    printf("\n\"1\"%d\n",count);
+    printf("\n%d\n",count);
     system("pause");
     return 0;
 }
-
