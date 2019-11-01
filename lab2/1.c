e92b322e8908f2e98a6a50e394398fe831739f49
NIKE-ADIDASA <a117163086@126.com>
2019-11-01 06:49:10 -0700

    added a 1.c file

---
+++ lab2/1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+   unsigned int i = 1;
+   int c = 0;
+   while (i !=0)
+   {
+     i = i << 1;
+     c =  c + 1;
+   }
+printf ("%d\n",c);
+unsigned int x = 0 - 1;
+printf("%u\n",x);
+return 0;
+}
