a8efc97572b99146cc20d480dde2ebbb4eb6aa04
NIKE-ADIDASA <a117163086@126.com>
2019-11-13 22:02:37 -0800

    added a ex1.c file

---
+++ lab3/ex1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int count(char *str){
+    int i = 0;
+    while(str[i++] !='\0'){
+        ;
+    }
+    i--;
+    return i;
+}
+int strrindex(char line[],char pattern[]){
+    int a = count(line);
+    int b = count(pattern);
+    
+    int i = a;
+    while(i !=1){
+      int k = 0;int j = i - b + 1;
+      while(j !=1 && line[j] ==  pattern[k]){
+      k++;j--;
+      }
+      i--;
+      if(k == b){
+          return i;
+         }
+      return -1;
+    }
+}
+main()
+{
+    char line[1000];
+    char pattern[1000];
+    scanf("%s %s",line,pattern);
+    printf("%d\n",strrindex(line,pattern));
+}
