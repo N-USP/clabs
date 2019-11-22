77926cd5faf10821eba68c89dc058393de0a1d5a
NIKE-ADIDASA <a117163086@126.com>
2019-11-22 14:44:35 -0800

    added a bc2.c file

--- lab2/bc2.c
+++ lab2/bc2.c
@@ -2,41 +2,18 @@
 #include <stdlib.h>
 int main()
 {
-    int c[4];
-    int b = 0;
-    int x;
-
-    c[b] = getchar();
-    while (c[b] !='\n'){
-        
-       c[b] = c[b] - '0';
-       b++;
-      c[b] = getchar();
+    int c;
+    int x = 0;
+    c = getchar();
+    while(c !='\n'){
+      c = c - '0';
+      x = x * 10 + c;
+      c = getchar();
     }
-    
-    if(b == 1)
-    {x = c[0];}
-
-    else if(b == 2)
-    {x = c[0]*10 + c[1];}
-    
-    else if(b == 3)
-    {x = c[0]*100 + c[1]*10 + c[2];}
-
-    else if(b == 4)
-    {x = c[0]*1000 + c[1]*100 + c[2]*10 + c[3];}
-
-    else if(b == 5)
-    {x = c[0]*10000 + c[1]*1000 + c[2]*100 + c[3]*10 + c[4];}
-
-    else{exit(0);}
-
-      int n = x;
       int count = 0;
-    while(n != 0) {
-        n = n & (n-1);
+    while(x != 0) {
+        x = x & (x-1);
         count++;
     }
     printf("%d\n",count);
-
 }
