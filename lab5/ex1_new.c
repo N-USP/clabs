e83a6e8c579976c2016e88b975bbb646634cd0c6
N-USP <56427908+N-USP@users.noreply.github.com>
2019-12-05 22:11:30 +0800

    Update

--- lab5/ex1_new.c
+++ lab5/ex1_new.c
@@ -1,28 +1,28 @@
 #include<stdio.h>
 int count(char str[]){
     int i = 0;
-    while(str[i++] !='\0'){
-        ;
+    while(str[i] != '\0'){
+      i++;
     }
     return i;
 }
 
 void reverse(char input[], char result[]) {
-    int length = count(s);
+    int length = count(input) - 1;
     int index = 0;
     while(length >= 0){
       result[index++] = input[length--];
     }
 }
-    
+
 int main()
 {
     char input[1024];
     int i = 0;
-    a[i] = getchar();
-    while (a[i] !='\n'){
+    input[i] = getchar();
+    while (input[i] !='\n'){
         i++;
-        a[i] = getchar();
+        input[i] = getchar();
     }
 
     char result[1024];
