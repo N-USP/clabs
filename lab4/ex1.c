0f7208b9fb3dacf25ef10702890d9880997dbbf8
N-USP <56427908+N-USP@users.noreply.github.com>
2019-12-06 18:08:35 +0800

    Update ex1.c

--- lab4/ex1.c
+++ lab4/ex1.c
@@ -1,123 +1,118 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
 
 #define MAXOP 100
 #define NUMBER '0'
-
-int getop(char []);
-void push(double);
-double pop(void);
-
-int main()
-{
-    int type;
-    double op2;
-    char s[MAXOP];
-
-    while((type = getop(s)) !=EOF){
-        switch(type){
-            case NUMBER :
-            push(atof(s));
-            break;
-        case'+':
-            push(pop() + pop());
-            break;
-        case '%': 
-            op2 = pop();
-            if (op2 != 0) 
-               push ((int)pop() % (int)op2); 
-            else printf("error : zero divisor\n"); 
-            break;
-        case'*':
-            push(pop() * pop());
-            break;
-        case'-':
-            op2 = pop();
-            push(pop() - op2);
-            break;
-        case'/':
-            op2 = pop();
-            if (op2 !=0.0)
-               push(pop() / op2);
-            else
-               printf("error:zero divisor\n");
-            break;
-        case'\n':
-            printf("\t%.8g\n",pop());
-            break;
-        default:
-            printf("error:unkown command %s\n",s);
-            break;
-        }
-    }
-    return 0;
-}
-
 #define MAXVAL 100
+#define BUFSIZE 100
 
 int sp = 0;
 double val[MAXVAL];
+char buf[BUFSIZ];
+int bufp = 0 ;
 
-void push(double f)
-{
-    if(sp < MAXVAL)
-    val[sp++] = f;
-    else
-    printf("error:stack full,can't push %g\n",f);
-    
-}
-
-double pop(void)
-{
-    if(sp > 0)
-    return val[--sp];
-    else{
-        printf("error:stack empty\n");
-        return 0.0;
-    }
-}
-
-#include<ctype.h>
-
+int getop ( char s[] );
+void push (double f);
+double pop (void);
 int getch(void);
-void ungetch(int);
+void ungetch( int c);
 
-int getop(char s[])
-{
-    int i,c;
+int main (){
+	int type;
+	double op2;
+	char s[MAXOP];
 
-    while((s[0] = c = getch()) == ' ' || c == '\t')
-    ;
-    s[1] = '\0';
-    if(!isdigit(c) && c !='.')
-      return c;
-    i = 0;
-    if(isdigit(c))
-      while(isdigit(s[++i] = c = getch()))
-    ;
-    if(c == '.')
-      while(isdigit(s[++i] = c = getch()))
-    ;
-    s[i] = '\0';
-    if(c !=EOF)
-      ungetch(c);
-    return NUMBER;
+	while ( ( type = getop( s )) != EOF ){
+		switch (type)
+		{
+			case NUMBER:
+				push( atof (s) );
+				break;
+			case '+':
+				push ( pop () + pop ());
+				break;
+			case '*':
+				push (pop() * pop() );
+				break;
+			case '-':
+				op2 = pop();
+				push ( pop() - op2);
+				break ;
+			case '/':
+				op2 = pop();
+				if( op2 != 0)
+					push (pop() / op2 );
+				else printf("error : zero divisor\n");
+				break;
+			case '%':
+				op2 = pop();
+                if(op2 != 0){
+				push((int)pop() % (int)op2);
+                }
+                else printf("error ; zero divisor\n");
+                break;
+			case '\n':
+				printf("result = %2.8g\n",pop());
+				break;
+			default:
+				printf("unknown commond %s\n",s);
+				break;
+		}
+	}
+	return 0;
 }
 
-#define BUFSIZE 100
-
-char buf[BUFSIZE];
-int bufp = 0;
-
-int getch(void)
-{
-    return(bufp > 0) ? buf[--bufp] : getchar();
+void push (double f){
+	if ( sp < MAXVAL)
+		val[sp++] = f;
+	else printf("error : stack full,can not push %g\n",f);
 }
 
-void ungetch(int c)
-{
-    if(bufp >= BUFSIZE)
-      printf("ungetch: too many characters\n");
-    else
-      buf[bufp++] = c;
+double pop (void){
+	if ( sp > 0)
+		return val[--sp];
+	else printf("error : stack empty!\n");
+    return 0.0;
+}
+int getop (char s[] ){
+	int i,c;
+	while ( (s[0] = c = getch()) == ' ' || c == '\t' )
+	 ;
+	s[1] = '\0';
+	if ( !isdigit(c) && c != '.' && c != '-')
+		return c;
+	i = 0;
+	if ( c == '-' ){
+		if ( isdigit(s[++i] = c = getch() ) || c == '.')
+			s[i] = c;
+		else {
+			if ( c != EOF )
+				ungetch(c);
+		return '-';
+		}
+	}	
+	if ( isdigit(s[i]) ) 
+		while ( isdigit(s[++i] = c = getch()) )
+		;
+	if ( c == '.' )
+		while ( isdigit(s[++i] = c = getch()) )
+		;
+	s[i] = '\0';
+	if ( c != EOF )
+		ungetch( c );
+	return NUMBER;
+}
+
+
+
+int getch(void){
+	return (bufp > 0) ? buf[--bufp] : getchar() ;
+}
+
+void ungetch( int c){
+	if ( bufp >= BUFSIZ)
+		printf("ungetch : too many characters!\n");
+	else buf[bufp++] = c ;
 }
